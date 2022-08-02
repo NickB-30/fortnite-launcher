2e71fecda94ed78f1595f673a8bab84a4548ff99
NickB301 <nicolas94010@gmail.com>
2022-08-02 16:36:53 -0700

    checks for invalid input

--- main.py
+++ main.py
@@ -5,29 +5,31 @@ from pyinjector import inject
 import psutil
 import time
 
-launch = input("press 1 to launch 7.40, 2 to quit:\n")
-if launch == "2":
-    sys.exit
-elif launch == "1":
-    print("Opening LawinServer")
-    os.chdir(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main')
-    p = subprocess.Popen(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main\start.bat')
-    print("Opening Fiddler")
-    p = subprocess.Popen(r'C:\Users\nicol\AppData\Local\Programs\Fiddler\Fiddler.exe')
-    os.chdir(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64')
-    p = subprocess.Popen(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64\Launcher.bat', creationflags=subprocess.CREATE_NEW_CONSOLE)
-    print("Launching 7.40")
-    time.sleep(48)
-    injected = False
-    while injected == False:
-        process_name = "Fortnite"
-        pid = None
-        for proc in psutil.process_iter():
-            if process_name in proc.name():
-                pid = proc.pid
-        inject(pid, r'C:\Users\nicol\Desktop\UniversalFNConsole.dll')
-        injected = True
-        print("Injected Console DLL")
-else:
-    print("?")
-    
+def start():
+    launch = input("press 1 to launch 7.40, 2 to quit:\n")
+    if launch == "2":
+        sys.exit
+    elif launch == "1":
+        print("Opening LawinServer")
+        os.chdir(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main')
+        p = subprocess.Popen(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main\start.bat')
+        print("Opening Fiddler")
+        p = subprocess.Popen(r'C:\Users\nicol\AppData\Local\Programs\Fiddler\Fiddler.exe')
+        os.chdir(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64')
+        p = subprocess.Popen(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64\Launcher.bat', creationflags=subprocess.CREATE_NEW_CONSOLE)
+        print("Launching 7.40")
+        time.sleep(48)
+        injected = False
+        while injected == False:
+            process_name = "Fortnite"
+            pid = None
+            for proc in psutil.process_iter():
+                if process_name in proc.name():
+                    pid = proc.pid
+            inject(pid, r'C:\Users\nicol\Desktop\UniversalFNConsole.dll')
+            injected = True
+            print("Injected Console DLL")
+    else:
+        print("Invalid Choice")
+        start()
+start()
