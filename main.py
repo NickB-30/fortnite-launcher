a50e75be33d28457d4f72faabd8816406754dab4
NickB301 <nicolas94010@gmail.com>
2022-08-02 12:42:50 -0700

    update sleep time

--- main.py
+++ main.py
@@ -11,19 +11,18 @@ if launch == "2":
 else:
     print("Opening LawinServer")
     os.chdir(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main')
-    p = subprocess.Popen(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main\start.bat', creationflags=subprocess.CREATE_NEW_CONSOLE)
+    p = subprocess.Popen(r'C:\Users\nicol\Desktop\normal-lawin\LawinServer-main\start.bat')
     print("Opening Fiddler")
     p = subprocess.Popen(r'C:\Users\nicol\AppData\Local\Programs\Fiddler\Fiddler.exe')
     print("Launching 7.40")
     os.chdir(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64')
     p = subprocess.Popen(r'C:\Users\nicol\Desktop\fortnite-builds\7.40\FortniteGame\Binaries\Win64\Launcher.bat', creationflags=subprocess.CREATE_NEW_CONSOLE)
     print("Launched 7.40")
-    time.sleep(40)
+    time.sleep(48)
     process_name = "Fortnite"
     pid = None
     for proc in psutil.process_iter():
         if process_name in proc.name():
             pid = proc.pid
-            print(pid)
     inject(pid, r'C:\Users\nicol\Desktop\UniversalFNConsole.dll')
     
